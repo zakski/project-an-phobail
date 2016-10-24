d2413b742e964bb654986bb2892f527f41031a34
zakski <zak.szadowski+git@gmail.com>
2016-10-25 00:26:03 +0100

    Added simple File Finder

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 A repository for all shared/common Scala code that somehow always makes it into all my other projects.
 
-naming convention comes from a loose english to irish translation of the word public.
+The Naming Convention of these projects comes from a loose english to irish translation of a word, in this case its the word 'public'.
+
+## IO
+The io packages are aimed simplifying the reading, writing and finding of files. Tests are not supplied for these classes, due there being no ironclad 
+guarantees of any test files existing.
+
+## Lang
+The lang portion of the project is designed to provide all the helpful string manipulation code i reuse throughout my projects, such as capitalisation of words.
---
+++ src/main/scala/com/szadowsz/common/io/explore/FileFinder.scala
@@ -0,0 +1,62 @@
+// Copyright 2016 zakski.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.szadowsz.common.io.explore
+
+import java.io.{File, FilenameFilter}
+
+/**
+  * Utility class to perform file finding drudgery tasks.
+  *
+  * @author Zakski : 16/09/2015.
+  */
+object FileFinder {
+
+  /**
+    * Method to get the list of files under the specified path, recursively if the filter allows it.
+    *
+    * @param dir    the directory to look at.
+    * @param filter optional file filter, null if not needed.
+    * @return an array of found files.
+    */
+  private def search(dir: File, filter: FilenameFilter): Array[File] = {
+    dir.listFiles(filter).flatMap(file => if (file.isDirectory) search(file, filter) else List(file))
+  }
+
+  /**
+    * Method to get the list of files under the specified path, recursively if possible.
+    *
+    * @param directoryPath the file path directory string to look at.
+    * @param filter        optional file filter.
+    * @return the list of found files.
+    */
+  def search(directoryPath: String, filter: Option[FilenameFilter] = None): Array[File] = search(new File(directoryPath), filter)
+
+
+  /**
+    * Method to get the list of files under the specified path, recursively if possible.
+    *
+    * @param directory the file directory to look at.
+    * @param filter    optional file filter.
+    * @return an array of found files.
+    */
+  def search(directory: File, filter: Option[FilenameFilter] = None): Array[File] = {
+    if (directory.exists() && directory.isDirectory) {
+      search(directory, filter.orNull)
+    } else {
+      Array()
+    }
+  }
+}
